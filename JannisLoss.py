9634894bf143e9d5997c65abb7724f23c58f03fe
stillsen <stillsen@gmail.com>
2020-05-01 17:54:16 +0200

    changed jannis loss to hierarchical multilabel loss, i.e. it picks one from 6 dimensions as label

--- JannisLoss.py
+++ JannisLoss.py
@@ -6,10 +6,11 @@ from mxnet.gluon.loss import _reshape_like
 class JannisLoss(Loss):
 
 
-    def __init__(self, axis=-1, sparse_label=True, from_logits=False, weight=None,
+    def __init__(self, rank, axis=-1, sparse_label=True, from_logits=False, weight=None,
                  batch_axis=0, **kwargs):
         super(JannisLoss, self).__init__(
             weight, batch_axis, **kwargs)
+        self.rank = rank
         self._axis = axis
         self._sparse_label = sparse_label
         self._from_logits = from_logits
@@ -18,13 +19,20 @@ class JannisLoss(Loss):
         if not self._from_logits:
             pred = F.log_softmax(pred, self._axis)
         if self._sparse_label:
-            loss_0 = F.pick(pred, label[:, 0], axis=self._axis, keepdims=True)
-            loss_1 = F.pick(pred, label[:, 1], axis=self._axis, keepdims=True)
-            loss_2 = F.pick(pred, label[:, 2], axis=self._axis, keepdims=True)
-            loss_3 = F.pick(pred, label[:, 3], axis=self._axis, keepdims=True)
-            loss_4 = F.pick(pred, label[:, 4], axis=self._axis, keepdims=True)
-            loss_5 = F.pick(pred, label[:, 5], axis=self._axis, keepdims=True)
-            loss = -1*(loss_0+loss_1+loss_2+loss_3+loss_4+loss_5)
+            if self.rank == 0:
+                loss = F.pick(pred, label[:, 0], axis=self._axis, keepdims=True)
+            if self.rank == 1:
+                loss = F.pick(pred, label[:, 1], axis=self._axis, keepdims=True)
+            if self.rank == 2:
+                loss = F.pick(pred, label[:, 2], axis=self._axis, keepdims=True)
+            if self.rank == 3:
+                loss = F.pick(pred, label[:, 3], axis=self._axis, keepdims=True)
+            if self.rank == 4:
+                loss = F.pick(pred, label[:, 4], axis=self._axis, keepdims=True)
+            if self.rank == 5:
+                loss = F.pick(pred, label[:, 5], axis=self._axis, keepdims=True)
+            loss = -loss
+            # loss = -1*(loss_0+loss_1+loss_2+loss_3+loss_4+loss_5)
         else:
             label = _reshape_like(F, label, pred)
             loss = -F.sum(pred * label, axis=self._axis, keepdims=True)
