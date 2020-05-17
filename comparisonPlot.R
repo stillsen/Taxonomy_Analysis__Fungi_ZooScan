5bc9d4b02084612e852874a985b7d91887111fd3
stillsen <stillsen@gmail.com>
2020-05-17 08:30:25 +0200

    added connecting line plot

--- comparisonPlot.R
+++ comparisonPlot.R
@@ -19,13 +19,15 @@ df <- read.table(abs_filename, sep=',', header=TRUE, stringsAsFactors = TRUE)
 # df <- df[,-1]
 # attach to search path for easier variable access
 attach(df)
+df$grp <- paste(df$Dataset, df$Model)
 
 df %>%
   arrange(Rank) %>%
   mutate(Rank = factor(Rank, levels=c("phylum", "class", "order", "family", "genus", "species")))%>%
   arrange(Rank)%>%
 ggplot()+
-  geom_point(aes_string(y="Score", x="Rank", colour="Model", shape="Dataset", group="Model"), size = 2.5, position =position_dodge(width = .25))
+  geom_point(aes_string(y="Score", x="Rank", colour="Model", shape="Dataset", group="grp"), size = 3.5, position =position_dodge(width = .25))+
+  geom_line(aes_string(y="Score", x="Rank", colour="Model", group="grp"), position =position_dodge(width = .25))
 
 file_name <- paste(path,'/',"comparisonPlot.pdf", sep="")
 ggsave(file_name)
